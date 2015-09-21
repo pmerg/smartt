b5ab973969f08c22b8fd39f45e33e4eacd6d3109
anargyros_tsadimas <tsadimas@gmail.com>
2015-09-21 11:03:23 +0300

    implemented api calls: routes, getroutesbystop, getnearlines, getnearstops, getlinesfromstop

---
+++ backend/application/controllers/api/Routes.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rg
+ * Date: 21/9/2015
+ * Time: 10:49 πμ
+ */
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Routes extends REST_Controller
+{
+    function __construct()
+    {
+        // Construct our parent class
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        parent::__construct();
+
+        // Configure limits on our controller methods. Ensure
+        // you have created the 'limits' table and enabled 'limits'
+        // within application/config/rest.php
+        $this->methods['user_get']['limit'] = 500; //500 requests per hour per user/key
+        $this->methods['user_post']['limit'] = 100; //100 requests per hour per user/key
+        $this->methods['user_delete']['limit'] = 50; //50 requests per hour per user/key
+    }
+
+
+    function routes_get()
+    {
+        $routes = $this->routes_model->get_routes();
+        if ($routes) {
+            $this->response($routes, 200); // 200 being the HTTP response code
+        } else {
+            $this->response(array('error' => 'Routes could not be found'), 404);
+        }
+
+    }
+
+    function routestops_get()
+    {
+        if (!$this->get('route') || !$this->get('dir')) {
+            $message = array('success' => 'false');
+            $this->response($message, 400);
+        }
+
+        $stops = $this->routes_model->get_stops_by_route($this->get('route'), $this->get('dir'));
+
+        if ($stops) {
+            $message = array('stops' => $stops, 'success' => 'true');
+            $this->response($message, 200);
+        }
+    }
+
+    function nearstops_get()
+    {
+        if (!$this->get('lat') || !$this->get('lon')) {
+            $message = array('success' => 'false');
+            $this->response($message, 400);
+        }
+
+        if (!$this->get('dist')) {
+            $dist = '0.2';
+        } else {
+            $dist = $this->get('dist');
+        }
+
+        $stops = $this->routes_model->get_nearest_stops($this->get('lat'), $this->get('lon'), $dist);
+        if ($stops) {
+            $message = array('stops' => $stops, 'success' => 'true');
+            $this->response($message, 200);
+        }
+    }
+
+    function nearlines_get()
+    {
+        //@todo get tracked
+        if (!$this->get('lat') || !$this->get('lon')) {
+            $message = array('success' => 'false');
+            $this->response($message, 400);
+        }
+
+        if (!$this->get('dist')) {
+            $dist = '0.2';
+        } else {
+            $dist = $this->get('dist');
+        }
+
+        $stops = $this->routes_model->get_nearest_lines($this->get('lat'), $this->get('lon'), $dist);
+        if ($stops) {
+            $message = array('lines' => $stops, 'success' => 'true');
+            $this->response($message, 200);
+        }
+    }
+
+    function linesfromstop_get()
+    {
+        if (!$this->get('stop')) {
+            $message = array('success' => 'false');
+            $this->response($message, 400);
+        }
+
+        $lines = $this->routes_model->findbusfromstop($this->get('stop'));
+        if ($lines) {
+            $message = array('lines' => $lines, 'success' => 'true');
+            $this->response($message, 200);
+        }
+    }
+}
--- backend/application/models/Routes_model.php
+++ backend/application/models/Routes_model.php
@@ -22,4 +22,136 @@ class Routes_model extends CI_Model
         log_message('info', $this->db->last_query());
         return $query->result();
     }
+
+    public function get_stops_by_route($route, $dir)
+    {
+
+        $sql = "select bus_stops.s_id, bus_stops.name_el, bus_stops.name_en, bus_stops.lat, bus_stops.lon, line_stops.direction_flag
+                from bus_stops, line_stops, bus_lines
+                where bus_stops.s_id=line_stops.stop_id  and line_stops.direction_flag = " . $dir . " and line_stops.line_id=bus_lines._id and bus_lines._id='" . $route . "'";
+
+
+        $query_response = $this->db->query($sql);
+        $result = $query_response->result();
+
+        if ($query_response->num_rows() < 1) {
+            return NULL;
+        } else {
+            return $result;
+        }
+
+        log_message('info', $this->db->last_query());
+        return $query->result();
+
+    }
+
+    public function get_nearest_stops($lat, $lon, $dist)
+    {
+
+        $sql = "SELECT  s_id,  name_el, name_en, street_el, street_en, lat, lon, (
+                    6371 * ACOS(COS(RADIANS(" . $lat . ")) * COS(RADIANS(lat)) * COS(RADIANS(lon) - RADIANS(" . $lon . ")) + SIN(RADIANS(" . $lat . ")) * SIN(RADIANS(lat)))
+                ) AS distance
+            FROM bus_stops
+            HAVING distance < " . $dist . "
+            ORDER BY distance";
+        $query_response = $this->db->query($sql);
+        log_message('info', $this->db->last_query());
+        $result = $query_response->result();
+
+        if ($query_response->num_rows() < 1) {
+            return NULL;
+        } else {
+            return $result;
+        }
+        return $query->result();
+    }
+
+    public function get_nearest_lines($lat, $lon, $dist)
+    {
+
+        $sql = "SELECT   line_id, direction_flag, line_name_el, line_name_en, (
+                    6371 * ACOS(COS(RADIANS(" . $lat . ")) * COS(RADIANS(lat)) * COS(RADIANS(lon) - RADIANS(" . $lon . ")) + SIN(RADIANS(" . $lat . ")) * SIN(RADIANS(lat)))
+                ) AS distance
+            FROM bus_stops, line_stops, bus_lines
+            where bus_stops.s_id = line_stops.stop_id and line_stops.line_id = bus_lines._id
+            HAVING distance < " . $dist . "
+            ORDER BY distance";
+        $query_response = $this->db->query($sql);
+        log_message('info', $this->db->last_query());
+        //$result = $query_response->result();
+        $result = array();
+        if ($query_response->num_rows() > 0) {
+            foreach ($query_response->result() as $row) {
+                $this->db->join('user_locations', 'bus_lines._id =  user_locations.routeid');
+                $dir=$row->direction_flag;
+                $this->db->where('direction',$dir);
+                $query1 = $this->db->get_where('bus_lines', array('_id' => $row->line_id));
+
+                $obj = new stdClass();
+
+                $obj->line_id = $row->line_id;
+                $obj->direction_flag = $row->direction_flag;
+                $obj->line_name_el = $row->line_name_el;
+                $obj->line_name_en = $row->line_name_en;
+                $obj->distance = $row->distance;
+                $obj->tracked = $query1->num_rows();
+
+                array_push($result, $obj);
+            }
+
+        }
+        return $result;
+
+
+        /* if ($query_response->num_rows() < 1) {
+             return NULL;
+         } else {
+             return $result;
+         }
+         return $query->result();*/
+    }
+
+    public function findbusfromstop($stop)
+    {
+
+        $result = array();
+        $this->db->select('line_stops.line_id');
+        $this->db->select('line_stops.direction_flag');
+        $this->db->select('bus_lines.line_name_el');
+        $this->db->select('bus_lines.line_name_en');
+        $this->db->select('bus_lines.is_circular');
+        $this->db->join('bus_lines', 'line_stops.line_id = bus_lines._id');
+        //$this->db->join('user_locations','line_stops.line_id =  user_locations.routeid');
+        $query = $this->db->get_where('line_stops', array('stop_id' => $stop));
+        //log_message('info', $this->db->last_query());
+
+
+        if ($query->num_rows() > 0) {
+            foreach ($query->result() as $row) {
+
+                $this->db->join('user_locations', 'bus_lines._id =  user_locations.routeid');
+                $dir=$row->direction_flag;
+                $this->db->where('direction',$dir);
+                $query1 = $this->db->get_where('bus_lines', array('_id' => $row->line_id));
+
+                $obj = new stdClass();
+
+                $obj->line_id = $row->line_id;
+                $obj->direction_flag = $row->direction_flag;
+                $obj->line_name_el = $row->line_name_el;
+                $obj->line_name_en = $row->line_name_en;
+                $obj->is_circular = $row->is_circular;
+                $obj->tracked = $query1->num_rows();
+
+                array_push($result, $obj);
+
+
+            }
+        }
+
+        //log_message('info', print_r($result));
+        //return $query->result();
+        return $result;
+    }
+
 }
